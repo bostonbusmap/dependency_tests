c8e3e5ceb278977bd9b34c66a1d48e0a5401e3a4
George Schneeloch <george_schneeloch@hms.harvard.edu>
2014-04-07 16:56:10 -0400

    Don't use internal git repository

--- run_on_examples.sh
+++ run_on_examples.sh
@@ -5,7 +5,6 @@ rm -rf venv-run-examples
 virtualenv venv-run-examples
 . venv-run-examples/bin/activate
 
-pip install git+ssh://grizzly/var/git/dependency_tests
-pip install nose
+python setup.py install
 
 venv-run-examples/bin/nosetests --nologcapture --with-dependency --with-xunit --xunit-file=./examples_out.xml --with-id --verbose examples
